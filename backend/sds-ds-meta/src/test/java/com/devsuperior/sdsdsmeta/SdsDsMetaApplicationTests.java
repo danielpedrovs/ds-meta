d549f18548506ad58b75c62a3b2c60134c2d388f
Daniel Sousa <danielpedrovs@gmail.com>
2022-07-13 15:05:49 +0100

    securityconfig

---
+++ backend/sds-ds-meta/src/test/java/com/devsuperior/sdsdsmeta/SdsDsMetaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devsuperior.sdsdsmeta;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SdsDsMetaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
