a662356558da7024403eaf85899d6c8597720ba8
Daniel Sousa <danielpedrovs@gmail.com>
2022-07-17 15:45:29 +0100

    Date Select

--- backend/sds-ds-meta/src/main/java/com/devsuperior/sdsdsmeta/controllers/SaleController.java
+++ backend/sds-ds-meta/src/main/java/com/devsuperior/sdsdsmeta/controllers/SaleController.java
@@ -1,10 +1,12 @@
 package com.devsuperior.sdsdsmeta.controllers;
-import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
 import com.devsuperior.sdsdsmeta.entities.Sale;
 import com.devsuperior.sdsdsmeta.services.SaleService;
 
@@ -15,9 +17,13 @@ public class SaleController {
 	@Autowired
 	private SaleService service;
 	
-	@GetMapping
-	public List <Sale> findSales(){
-		return service.findSales();
+	@GetMapping			
+	public  Page<Sale> findSales( 
+			@RequestParam(value="minDate", defaultValue="") String minDate,
+			@RequestParam(value="maxDate", defaultValue="") String maxDate, 
+			Pageable pageable){
+		
+		return service.findSales(minDate, maxDate, pageable);
 		
 	}
 }
